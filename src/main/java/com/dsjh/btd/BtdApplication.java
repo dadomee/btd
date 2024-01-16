9e18962d968f4ccc97c52f52a5e8d574c9153f24
dadomee <dasomi0021@knou.ac.kr>
2024-01-16 22:50:52 +0900

    initial

---
+++ src/main/java/com/dsjh/btd/BtdApplication.java
@@ -0,0 +1,13 @@
+package com.dsjh.btd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtdApplication.class, args);
+	}
+
+}
