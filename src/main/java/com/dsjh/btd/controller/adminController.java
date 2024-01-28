37a7bd08e05facb1cfe82f245d0aa24d32a5adc5
dadomee <dasomi0021@knou.ac.kr>
2024-01-28 14:45:27 +0900

    수정 - 컨트롤러

--- src/main/java/com/dsjh/btd/controller/adminController.java
+++ src/main/java/com/dsjh/btd/controller/adminController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-//@Autowired  adminService adminService;
+
+@Autowired
+    private adminService adminService;
 @Controller
 public class adminController {
 
