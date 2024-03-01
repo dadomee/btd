7f28232127ae309e10b135c1d1d7e9fd1a26c80a
s2szs2 <bc10237@gmail.com>
2024-03-01 19:10:29 +0900

    수정 - jsp, dto 수정 추가 - profDataBoard.jsp, listQnABoard.jsp, profQnABoard.jsp

--- src/main/java/com/dsjh/btd/controller/ProfessorController.java
+++ src/main/java/com/dsjh/btd/controller/ProfessorController.java
@@ -51,6 +51,24 @@ public class ProfessorController {
         return mav;
     }
 
+    // 자료실
+    @GetMapping("/professor/profDataBoard")
+    public ModelAndView professorDataBoard(int prof_id) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("prof_id",prof_id);
+        mav.setViewName("professor/profDataBoard");
+        return mav;
+    }
+
+    // 학습상담
+    @GetMapping("/professor/profQnABoard")
+    public ModelAndView professorQnABoard(int prof_id) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("prof_id",prof_id);
+        mav.setViewName("professor/profQnABoard");
+        return mav;
+    }
+
     // 교수 전용 페이지
     @GetMapping("/professor/profManagement")
     public ModelAndView professorManagement(int prof_id) {
@@ -86,6 +104,7 @@ public class ProfessorController {
         return mav;
     }
 
+    // 교수 전용 페이지 - 자료실 등록 페이지
     @RequestMapping(value = "/professor/writeDataBoard", method={RequestMethod.GET})
     public ModelAndView writeDataBoard(int prof_id, HttpServletRequest req) throws IOException {
         int num = 0, re_group = 0, re_step = 0, re_level = 0;
@@ -107,79 +126,80 @@ public class ProfessorController {
         return mav;
     }
 
+    // 교수 전용 페이지 - 자료실 등록 페이지
     @RequestMapping(value = "/professor/writeDataBoard", method={RequestMethod.POST})
     public ModelAndView writeDataBoard(int prof_id, HttpServletRequest req, @RequestParam("filename") List<MultipartFile> multiFileList, @ModelAttribute DataBoardDTO dto, BindingResult result) throws IOException {
-            if (result.hasErrors()) {
+//            if (result.hasErrors()) {
 //                dto.setDBoard_img1("");
-//                dto.setDBoard_img2("");
+//               dto.setDBoard_img2("");
 //                dto.setDBoard_img3("");
 //                dto.setDBoard_img4("");
-                dto.setDBoard_file1("");
-                dto.setDBoard_file2("");
-                dto.setDBoard_file3("");
-                dto.setDBoard_file4("");
-            }
-        int num = 0, re_group = 0, re_step = 0, re_level = 0;
-        //이미지 받기
-        MultipartHttpServletRequest mr = (MultipartHttpServletRequest) req;
-        MultipartFile mf = mr.getFile("board_img1");
-        MultipartFile mf2 = mr.getFile("board_img2");
-        MultipartFile mf3 = mr.getFile("board_img3");
-        MultipartFile mf4 = mr.getFile("board_img4");
-
-        String dBoard_file1 = mf.getOriginalFilename();
-        String dBoard_file2 = mf2.getOriginalFilename();
-        String dBoard_file3 = mf3.getOriginalFilename();
-        String dBoard_file4 = mf4.getOriginalFilename();
-
-        UUID uuid = UUID.randomUUID(); //이미지 중복 시 엑스박스 방지용 랜덤 파일명
-
-        HttpSession session= req.getSession();
-        String upPath = session.getServletContext().getRealPath("/resources/img");
-        String upPath1 = session.getServletContext().getRealPath("/resources/files");
-        session.setAttribute("upPath1", upPath1);
-        session.setAttribute("upPath", upPath);
-
-        DataBoardFileDTO fdto = new DataBoardFileDTO();
-
-        if (!mf.isEmpty()) { // 이미지 첨부 되어있을 시
-            dBoard_file1 = uuid.toString() + "_" + dBoard_file1;
-            File file1 = new File(upPath, dBoard_file1);
-            mf.transferTo(file1);
-            dto.setDBoard_file1(dBoard_file1);
-
-        } else if (mf.isEmpty()) { // 이미지 첨부 안되어있을시
-            dto.setDBoard_file1("");
-        }
-
-        if (!mf2.isEmpty()) {
-            dBoard_file2 = uuid.toString() + "_" + dBoard_file2;
-            File file2 = new File(upPath, dBoard_file2);
-            mf2.transferTo(file2);
-            dto.setDBoard_file2(dBoard_file2);
-
-        } else if (mf2.isEmpty()) {
-            dto.setDBoard_file2("");
-        }
-        if (!mf3.isEmpty()) {
-            dBoard_file3 = uuid.toString() + "_" + dBoard_file3;
-            File file3 = new File(upPath, dBoard_file3);
-            mf3.transferTo(file3);
-            dto.setDBoard_file3(dBoard_file3);
-        } else if (mf3.isEmpty()) {
-            dto.setDBoard_file3("");
-        }
-
-        if (!mf4.isEmpty()) {
-            dBoard_file4 = uuid.toString() + "_" + dBoard_file4;
-            File file4 = new File(upPath, dBoard_file4);
-            mf4.transferTo(file4);
-            dto.setDBoard_file4(dBoard_file4);
-        } else {
-            dto.setDBoard_file4("");
-        }
-
-        req.setAttribute("dto", dto);
+//                dto.setDBoard_file1("");
+//                dto.setDBoard_file2("");
+//                dto.setDBoard_file3("");
+//                dto.setDBoard_file4("");
+//            }
+//        int num = 0, re_group = 0, re_step = 0, re_level = 0;
+//        //이미지 받기
+//        MultipartHttpServletRequest mr = (MultipartHttpServletRequest) req;
+//        MultipartFile mf = mr.getFile("board_img1");
+//        MultipartFile mf2 = mr.getFile("board_img2");
+//        MultipartFile mf3 = mr.getFile("board_img3");
+//        MultipartFile mf4 = mr.getFile("board_img4");
+//
+//        String dBoard_file1 = mf.getOriginalFilename();
+//        String dBoard_file2 = mf2.getOriginalFilename();
+//        String dBoard_file3 = mf3.getOriginalFilename();
+//        String dBoard_file4 = mf4.getOriginalFilename();
+//
+//        UUID uuid = UUID.randomUUID(); //이미지 중복 시 엑스박스 방지용 랜덤 파일명
+//
+//        HttpSession session= req.getSession();
+//        String upPath = session.getServletContext().getRealPath("/resources/img");
+//        String upPath1 = session.getServletContext().getRealPath("/resources/files");
+//        session.setAttribute("upPath1", upPath1);
+//        session.setAttribute("upPath", upPath);
+//
+//        DataBoardFileDTO fdto = new DataBoardFileDTO();
+//
+//        if (!mf.isEmpty()) { // 이미지 첨부 되어있을 시
+//            dBoard_file1 = uuid.toString() + "_" + dBoard_file1;
+//            File file1 = new File(upPath, dBoard_file1);
+//            mf.transferTo(file1);
+//            dto.setDBoard_file1(dBoard_file1);
+//
+//        } else if (mf.isEmpty()) { // 이미지 첨부 안되어있을시
+//            dto.setDBoard_file1("");
+//        }
+//
+//        if (!mf2.isEmpty()) {
+//            dBoard_file2 = uuid.toString() + "_" + dBoard_file2;
+//            File file2 = new File(upPath, dBoard_file2);
+//            mf2.transferTo(file2);
+//            dto.setDBoard_file2(dBoard_file2);
+//
+//        } else if (mf2.isEmpty()) {
+//            dto.setDBoard_file2("");
+//        }
+//        if (!mf3.isEmpty()) {
+//            dBoard_file3 = uuid.toString() + "_" + dBoard_file3;
+//            File file3 = new File(upPath, dBoard_file3);
+//            mf3.transferTo(file3);
+//            dto.setDBoard_file3(dBoard_file3);
+//        } else if (mf3.isEmpty()) {
+//            dto.setDBoard_file3("");
+//        }
+//
+//        if (!mf4.isEmpty()) {
+//            dBoard_file4 = uuid.toString() + "_" + dBoard_file4;
+//            File file4 = new File(upPath, dBoard_file4);
+//            mf4.transferTo(file4);
+//            dto.setDBoard_file4(dBoard_file4);
+//        } else {
+//            dto.setDBoard_file4("");
+//        }
+//
+//        req.setAttribute("dto", dto);
 
 //        int res = professorService.insertBoard(dto);
 //        if (res > 0) {
@@ -248,11 +268,21 @@ public class ProfessorController {
         return mav;
     }
 
+    // 교수 전용 페이지 - 자료실 목록 페이지
     @GetMapping("/professor/listDataBoard")
     public ModelAndView listDataBoard(int prof_id) {
         ModelAndView mav = new ModelAndView();
         mav.addObject("prof_id", prof_id);
         mav.setViewName("professor/listDataBoard");
+        return mav;
+    }
+
+    // 교수 전용 페이지 - 학습상담 목록 페이지
+    @GetMapping("/professor/listQnABoard")
+    public ModelAndView listQnABoard(int prof_id) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("prof_id", prof_id);
+        mav.setViewName("professor/listQnABoard");
         return mav;
     }
 }
